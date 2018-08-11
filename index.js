45b566008efe7ab88bbe2620c266e08a44f4dd42
Pony <cuis@vip.qq.com>
2018-08-11 23:59:39 +0800

    first commit.

---
+++ index.js
@@ -0,0 +1,119 @@
+var browserify = require('browserify')
+var watchify = require('watchify')
+var tsify = require('tsify')
+var through = require('through')
+var fs = require('fs')
+var path = require('path')
+var strip_json_comments = require('strip-json-comments')
+
+class ResBundler {
+
+    constructor() {
+        this.contentCache = {}
+    }
+
+    files(dir) {
+        var files = []
+        try {
+            var results = fs.readdirSync(dir)
+            results.forEach(it => {
+                if (it == ".DS_Store") { return }
+                var subPaths = this.files(dir + "/" + it)
+                if (subPaths instanceof Array) {
+                    subPaths.forEach(it => {
+                        files.push(it)
+                    })
+                }
+                else {
+                    files.push(dir + "/" + it)
+                }
+            })
+        } catch (error) {
+            return null
+        }
+        return files
+    }
+
+    bundle() {
+        const files = this.files('res') || []
+        return files.map((it) => {
+            const content = this.contentCache[it] || fs.readFileSync(it).toString('base64')
+            this.contentCache[it] = content
+            return `Bundle.js["${it.replace('res/', '')}"] = "${content}";`
+        }).join("\n")
+    }
+
+}
+
+class SrcBundler {
+
+    compilerOptions() {
+        try {
+            const tsconfig = fs.readFileSync('tsconfig.json', { encoding: "utf-8" })
+            return JSON.parse(strip_json_comments(tsconfig)).compilerOptions
+        } catch (error) {
+            return {}
+        }
+    }
+
+    createBrowserify() {
+        return browserify({
+            cache: {},
+            packageCache: {},
+        })
+            .add('src/main.ts')
+            .plugin(tsify, this.compilerOptions())
+            .transform(function (file) {
+                var data = '';
+                return through(write, end);
+                function write(buf) { data += buf }
+                function end() {
+                    if (file == path.resolve('src/main.ts')) {
+                        data = resBundler.bundle() + "\n" + data
+                    }
+                    this.queue(data);
+                    this.queue(null);
+                }
+            })
+            .transform('uglifyify', { sourceMap: false })
+    }
+
+    watch() {
+        const b = this.createBrowserify()
+        b.plugin(watchify)
+            .on('update', () => {
+                b.bundle()
+                    .on('error', (error) => {
+                        console.error(error)
+                        process.exit(-1);
+                    })
+                    .pipe(fs.createWriteStream('./build/app.js'));
+                console.log("Built at: " + new Date())
+            })
+        b.bundle()
+            .on('error', console.error)
+            .pipe(fs.createWriteStream('./build/app.js'));
+        console.log("Built at: " + new Date())
+    }
+
+    build() {
+        const b = this.createBrowserify()
+        b.bundle()
+            .pipe(fs.createWriteStream('./build/app.js'));
+        console.log("Built at: " + new Date())
+    }
+
+}
+
+const resBundler = new ResBundler()
+const srcBundler = new SrcBundler()
+
+if (process.argv.includes('build')) {
+    srcBundler.build()
+}
+else if (process.argv.includes('watch')) {
+    srcBundler.watch()
+}
+else {
+    srcBundler.build()
+}
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+/// <reference path="../typings/index.d.ts" />
+
+const aView = new UIView
+aView.frame = { x: 0, y: 0, width: 100, height: 100 }
