6da59cde820f7ca249f2111546f21d6b57ee687e
PonyCui <cuis@vip.qq.com>
2018-08-13 19:44:47 +0800

    ...

--- types/kimi.d.ts
+++ types/kimi.d.ts
@@ -3,9 +3,9 @@ declare class KMCore {
 }
 
 declare class console {
-    log(message?: any, ...optionalParams: any[]): void;
-    info(message?: any, ...optionalParams: any[]): void;
-    warn(message?: any, ...optionalParams: any[]): void;
-    error(message?: any, ...optionalParams: any[]): void;
-    debug(message?: any, ...optionalParams: any[]): void;
+    static log(message?: any, ...optionalParams: any[]): void;
+    static info(message?: any, ...optionalParams: any[]): void;
+    static warn(message?: any, ...optionalParams: any[]): void;
+    static error(message?: any, ...optionalParams: any[]): void;
+    static debug(message?: any, ...optionalParams: any[]): void;
 }
