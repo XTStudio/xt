84db702e4f8f5043625f680c88bb2ed397681622
PonyCui <cuis@vip.qq.com>
2019-02-14 13:32:29 +0800

    fix: Don't add debug patch while debug on wx.

--- cli/src-bundler.js
+++ cli/src-bundler.js
@@ -208,6 +208,9 @@ class SrcBundler {
     }
     debugPatch() {
         if (process.argv.indexOf("debug") >= 0) {
+            if (process.argv.indexOf("--wx") > 0 || process.argv.indexOf("wx") > 0) {
+                return;
+            }
             try {
                 let data = fs.readFileSync(this.dist, { encoding: "utf-8" });
                 const debuggerScript = fs.readFileSync(path.resolve('node_modules', 'tiny-debugger', 'client/index.js'), { encoding: "utf-8" });
--- cli/src-bundler.ts
+++ cli/src-bundler.ts
@@ -209,6 +209,7 @@ export class SrcBundler {
 
     private debugPatch() {
         if (process.argv.indexOf("debug") >= 0) {
+            if (process.argv.indexOf("--wx") > 0 || process.argv.indexOf("wx") > 0) { return }
             try {
                 let data = fs.readFileSync(this.dist, { encoding: "utf-8" })
                 const debuggerScript = fs.readFileSync(path.resolve('node_modules', 'tiny-debugger', 'client/index.js'), { encoding: "utf-8" })
