2e650f3caf1bd4738aec07fc0a57923df2de5773
PonyCui <cuis@vip.qq.com>
2019-02-14 13:27:52 +0800

    feat: Add npm i command to WXRunner.

--- runner/wx.js
+++ runner/wx.js
@@ -16,6 +16,7 @@ class WXRunner {
         return __awaiter(this, void 0, void 0, function* () {
             try {
                 this.checkDevtools();
+                this.installNPM();
                 this.startProject();
             }
             catch (error) {
@@ -28,10 +29,14 @@ class WXRunner {
             throw Error("微信开发者工具尚未安装 @see https://developers.weixin.qq.com/miniprogram/dev/");
         }
     }
+    installNPM() {
+        console.log("正在加载依赖包");
+        child_process.execSync('npm i', { cwd: 'platform/wx/' });
+    }
     startProject() {
         console.log("正在启动微信开发者工具");
         console.log("如果项目没有成功启动，请检查 platform/wx/project.config.json 中的 appid 是否正确，你可以到微信开发者平台申请测试号并获取 appid。 @see https://developers.weixin.qq.com/sandbox");
-        const toolProcess = child_process.exec('/Applications/wechatwebdevtools.app/Contents/MacOS/cli -o ' + path_1.resolve('platform', 'wx'), { cwd: './' });
+        const toolProcess = child_process.exec('/Applications/wechatwebdevtools.app/Contents/MacOS/cli --build-npm ' + path_1.resolve('platform', 'wx') + ' -o ' + path_1.resolve('platform', 'wx'), { cwd: './' });
         toolProcess.stdout.on("data", (data) => {
             console.log(data);
         });
--- runner/wx.ts
+++ runner/wx.ts
@@ -7,6 +7,7 @@ export class WXRunner {
     async run() {
         try {
             this.checkDevtools()
+            this.installNPM()
             this.startProject()
         } catch (error) {
             console.error(error)
@@ -19,10 +20,15 @@ export class WXRunner {
         }
     }
 
+    installNPM() {
+        console.log("正在加载依赖包")
+        child_process.execSync('npm i', { cwd: 'platform/wx/' })
+    }
+
     startProject() {
         console.log("正在启动微信开发者工具")
         console.log("如果项目没有成功启动，请检查 platform/wx/project.config.json 中的 appid 是否正确，你可以到微信开发者平台申请测试号并获取 appid。 @see https://developers.weixin.qq.com/sandbox")
-        const toolProcess = child_process.exec('/Applications/wechatwebdevtools.app/Contents/MacOS/cli -o ' + resolve('platform', 'wx'), { cwd: './' })
+        const toolProcess = child_process.exec('/Applications/wechatwebdevtools.app/Contents/MacOS/cli --build-npm ' + resolve('platform', 'wx') + ' -o ' + resolve('platform', 'wx'), { cwd: './' })
         toolProcess.stdout.on("data", (data: any) => {
             console.log(data)
         })
